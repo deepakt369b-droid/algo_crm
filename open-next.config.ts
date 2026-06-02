0fb157d621699f0d8eadb2e94f3f7227deb89939
projects-jpg <deepakt369b@gmail.com>
2026-06-02 23:57:18 +0400

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -9,15 +9,13 @@ export default {
       queue: "dummy",
     },
   },
+  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
       wrapper: "cloudflare-edge",
       converter: "edge",
       proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
     },
   },
 };
